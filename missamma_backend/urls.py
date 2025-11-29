002cf3ade9b47fe5d5dd2ae337f5d8795ff1473a
E CHARAN <charanedamalapati2005@gmail.com>
2025-11-29 10:44:07 +0530

    first commit

--- missamma_backend/urls.py
+++ missamma_backend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/store/', include('store.urls')),
     path('api/payments/', include('payments.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
